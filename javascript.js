8928e7efb5e32ed91c20b414cdf3471c9879845b
Ramil <89657343+ramiiil@users.noreply.github.com>
2023-03-08 09:49:41 +0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,7 @@
+$(".p1").parallax({ imageSrc: "./images/helix.jpg" });
+
+$(".p2").parallax({ imageSrc: "./images/reflection.jpg" });
+
+$(".p3").parallax({ imageSrc: "./images/spiral.jpg" });
+
+$(".p4").parallax({ imageSrc: "./images/stellar.jpg" });
